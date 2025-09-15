25809595335d97de0e40645aa376c8ab4718d1a0
mutalisk999 <81849682@qq.com>
2025-09-16 01:06:38 +0800

    Air Temperature Plot

---
+++ air_temp_plot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from typing import List
+import matplotlib.pyplot as plt
+
+from weather_api import WeatherApi
+
+
+def plot(date_list: List, high_list: List, low_list: List):
+    fig, ax = plt.subplots()
+    ax.plot(date_list, high_list, date_list, low_list)
+    ax.set_title('nanjing')
+    ax.set_xlabel('date')
+    ax.set_ylabel('high/low temp ℃')
+    ax.grid(True)
+    plt.show()
+
+
+def main():
+    wApi = WeatherApi()
+    r = wApi.get_by_city_name("南京")
+    date_list, high_list, low_list = WeatherApi.parse_forecast_list(r)
+
+    # date_list = ['2025-09-15', '2025-09-16', '2025-09-17', '2025-09-18', '2025-09-19', '2025-09-20', '2025-09-21',
+    #              '2025-09-22',
+    #              '2025-09-23', '2025-09-24', '2025-09-25', '2025-09-26', '2025-09-27', '2025-09-28', '2025-09-29']
+    # high_list = [32, 34, 30, 27, 28, 28, 29, 34, 34, 32, 26, 26, 26, 27, 30]
+    # low_list = [25, 25, 24, 22, 22, 22, 22, 25, 25, 25, 22, 19, 16, 16, 20]
+
+    plot(date_list, high_list, low_list)
+
+
+if __name__ == "__main__":
+    main()
---
+++ weather_api.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from typing import Optional, Dict, Tuple, List
+import json
+import requests
+
+
+class WeatherApi(object):
+    def __init__(self):
+        self.url_tpl = "http://t.weather.itboy.net/api/weather/city/{}"
+        self.session = requests.Session()
+        self.session.trust_env = False
+
+        obj = json.loads(open("city.json", "r", encoding="utf-8").read())
+        self.city_map = dict()
+        for province in obj.get("城市代码"):
+            for city in province.get("市"):
+                self.city_map[city.get("市名")] = city.get("编码")
+
+    def get_by_city_key(self, city_key: str) -> Optional[Dict]:
+        url = self.url_tpl.format(city_key)
+        resp = self.session.get(url)
+        return resp.json()
+
+    def get_by_city_name(self, city_name: str) -> Optional[Dict]:
+        city_key = self.city_map.get(city_name)
+        if city_key is None:
+            return None
+
+        return self.get_by_city_key(city_key=city_key)
+
+    @staticmethod
+    def parse_forecast_list(resp_dict: Dict) -> Tuple[List, List, List]:
+        forecast_list = resp_dict.get("data").get("forecast")
+        date_list = list(map(lambda x: x.get("ymd"), forecast_list))
+        high_list = list(map(lambda x: int(x.get("high").split()[1].rstrip("℃")), forecast_list))
+        low_list = list(map(lambda x: int(x.get("low").split()[1].rstrip("℃")), forecast_list))
+        return date_list, high_list, low_list
+
+
+if __name__ == "__main__":
+    wApi = WeatherApi()
+    r = wApi.get_by_city_name("南京")
+    print(r)
+
+    l = WeatherApi.parse_forecast_list(r)
+    print(l)
